2bdb2800f7e65ac1831d7f1c259a69e2393ca3bc
Matheus Sforça <matheus.sforca@dextra-sw.com>
2021-01-20 14:28:47 -0300

    Projeto inicial

---
+++ apigraphql/src/test/java/com/dextra/apigraphql/ApigraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dextra.apigraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
